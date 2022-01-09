62c65db531561315c59c9336db3361fe234f3883
zammie01 <adenijioluseguntope@gmail.com>
2022-01-09 21:17:06 +0100

    I added more funtionallity

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-let firstCard = 6;
-let secondCard = 10;
-
+let firstCard = getRandomNumbers();
+let secondCard = getRandomNumbers();
+let cards = [firstCard, secondCard];
 let sum  = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true; 
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
 
-if (sum < 21) {
-    console.log("You have to pick another card 😊");
-} else if (sum === 21) {
-    console.log("You have gotten a blackjack! 🙌");
-} else {
-    console.log("You have lost! 😢");
+
+function getRandomNumbers() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    return randomNumber;
+}
+
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+
+    cardEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " ";
+    }
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20) {
+        message = "You have to pick  another card 😊";
+    } else if (sum === 21) {
+        message = "You have gotten a blackjack! 🙌";
+                hasBlackJack = true;
+    } else {
+        message = "You have lost! 😢";
+        isAlive = false;
+    }
+    
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    let card = getRandomNumbers();
+    sum += card;
+    cards.push(card);
+    renderGame();
 }
