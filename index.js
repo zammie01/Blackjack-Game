670a290bbd4d3bfce1a5f11cdc48b3d79a7d8b8a
zammie01 <adenijioluseguntope@gmail.com>
2022-01-11 09:10:56 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-let firstCard = getRandomNumbers();
-let secondCard = getRandomNumbers();
-let cards = [firstCard, secondCard];
-let sum  = firstCard + secondCard;
+let cards = [];
+let sum  = 0;
 let hasBlackJack = false;
-let isAlive = true; 
+let isAlive = false; 
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.getElementById("card-el");
 
+let player = {
+    name: "Samuel",
+    chips: "100"
+}
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": " + "$" + player.chips;
 
 function getRandomNumbers() {
     let randomNumber = Math.floor(Math.random() * 13) + 1;
-    return randomNumber;
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    } 
 }
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomNumbers();
+    let secondCard = getRandomNumbers();
+    cards = [firstCard, secondCard];
+    sum  = firstCard + secondCard;
     renderGame();
 }
 
@@ -41,8 +57,10 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomNumbers();
-    sum += card;
-    cards.push(card);
-    renderGame();
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomNumbers();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
 }
